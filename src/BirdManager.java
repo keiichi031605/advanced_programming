cc98ed7e353c5ef14a098328ca146f503e483de8
keiichi031605 <keichi031605@gmail.com>
2022-03-09 20:17:38 +1100

    updated

---
+++ src/BirdManager.java
@@ -0,0 +1,27 @@
+
+// Managing Bird class
+import java.util.ArrayList; 
+public class BirdManager {
+  public static void main(String[] args) {
+//    ArrayList<Bird> bird_database = new ArrayList<Bird>();
+//
+//    Bird bird_a = new Bird("general bird", "white");
+//    bird_database.add(bird_a);
+//    // System.out.println(bird_a.greeting());
+//    // System.out.println(bird_a.fly());
+//    
+//    Eagle bird_b = new Eagle("Keiichi", "red");
+//    bird_database.add(bird_b);
+//    // System.out.println(bird_b.greeting());
+//    // System.out.println(bird_b.fly());
+//
+//    
+//    for (int i = 0; i < bird_database.size(); i++) {
+//      Bird bird = bird_database.get(i);
+//      System.out.println(bird.greeting());
+//      System.out.println(bird.fly());
+//    }
+// google other way of for loop in java as well!!
+
+  }
+}
