615c03225e8efb462ed516175e549a68c93ed3ee
keiichi031605 <keichi031605@gmail.com>
2022-03-10 08:52:24 +1100

    created software engineer class

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
 
+import java.util.ArrayList; 
 public class Main {
 
 public static void main(String[] args) {
-  // TODO Auto-generated method stub
-    Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
-    person1.printData();
+
+    ArrayList<Person> employees = new ArrayList<Person>(); // Create an ArrayList object
+
+    Person person1 = new Person("Keiichi", "Katsuno", 31, 1.7, 75.0);
+    // person1.printData();
+    employees.add(person1);
     
-    Person person2 = new Person("John", "Smith", 65, 1.75, 80.0);
-    person2.printData();
+    Person person2 = new Person("Jinkoo", "Kang", 35, 1.75, 80.0);
+    // person2.printData();
+    employees.add(person2);
+
+    SoftwareEngineer person3 = new SoftwareEngineer("Reece", "Arms", 28, 1.7, 65);
+    employees.add(person3);
+
+    for (int i = 0; i < employees.size(); i++) {
+      Person employee = employees.get(i);
+      employee.printData();
+    }
+
     
     Person.printCount();
 	}
