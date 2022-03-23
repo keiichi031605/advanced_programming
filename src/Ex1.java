ba100ea3b0f54343379c8c0621e9fbbba5b7ba83
keiichi031605 <keichi031605@gmail.com>
2022-03-23 15:49:36 +1100

    submitted code exercise

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,28 +1,28 @@
 
 import java.util.ArrayList;
-import java.util.Collections;
 public class Ex1 {
 
 	public static void main(String[] args) {
     ArrayList<Integer> myArrayList = new ArrayList<Integer>();
-    myArrayList.add(3);
-    myArrayList.add(1);
-    myArrayList.add(7);
+    myArrayList.add(-5);
     myArrayList.add(20);
-    myArrayList.add(5);
+    myArrayList.add(0);
+    myArrayList.add(100);
 
-    // for (int i = 0; i < myArrayList.size(); i++) {
-    //   int number = myArrayList.get(i);
-    //   System.out.println(number);
-    // }
-    printMinMax(myArrayList);
+    printAverage(myArrayList);
   }
 
-  public static void printMinMax(ArrayList<Integer> numbers) {
-    int maxValue = Collections.max(numbers);
-    System.out.println("Max value is: " + maxValue);
-
-    int minValue = Collections.min(numbers);
-    System.out.println("Minimum value is: " + minValue);
+  public static void printAverage(ArrayList<Integer> numbers) {
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) > -1) {
+        sum = sum + numbers.get(i);
+        count = count + 1;
+      }
+    }
+    double av = sum/count;
+    
+    System.out.println("Average value is: " + av);
   }
 }
