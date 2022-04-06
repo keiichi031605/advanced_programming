564ebd699e048fae9c7fa76bc17a749384ea349a
keiichi031605 <keichi031605@gmail.com>
2022-04-06 16:52:16 +1000

    practiced JUnit with Calculator.java

--- src/KeiichiJunit/CalculatorTest.java
+++ src/KeiichiJunit/CalculatorTest.java
@@ -9,26 +9,20 @@ import week4.Calculator;
 
 class CalculatorTest {
 
-	// @Test
-	// void test() {
-	// 	fail("Not yet implemented");
-	// }
   @Test
-  public void maltiplicationで5と2の乗算結果が取得できる() throws Exception {
+  public void testMultiplication() throws Exception {
       Calculator sut = new Calculator();
       int expected = 10;
-      int actual = sut.maltiplication(5, 2);
+      int actual = sut.maltiplication(50, 2);
 //      assertThat(actual,is(expected));
   }
 
   @Test
-  public void divisionで5と8の除算結果が取得できる() throws Exception {
+  public void testDivision() throws Exception {
       Calculator sut = new Calculator();
       float expected = 0.625f;
       float actual =  sut.division(5, 8);
 //      assertThat(actual,is(expected));
   }
-
-
 
 }
